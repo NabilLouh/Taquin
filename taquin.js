69a8de7867eebb856de851eb669ee87e23d02e65
Arkhin <nabillouh62110@gmail.com>
2022-05-30 09:50:15 +0200

    correction erreur counthit

--- taquin.js
+++ taquin.js
@@ -135,123 +135,122 @@ $('#debut').click(function() {
 
 $('.case').click(function() {
     if (lancer == true) {
-        counthit = counthit + 1
-    $('.count').text("Nombre de coup : " + counthit);
+       
+        caseclickTop = $(this).position().top
+        caseclickLeft = $(this).position().left
 
-   
-    caseclickTop = $(this).position().top
-    caseclickLeft = $(this).position().left
-
-    
+        
 
 
-    if ((caseclickTop + 100 == casevidetop && caseclickLeft == casevideleft) 
-    || (caseclickTop - 100 == casevidetop && caseclickLeft == casevideleft)
-    || (caseclickLeft - 100 == casevideleft && caseclickTop == casevidetop)
-    || (caseclickLeft + 100 == casevideleft && caseclickTop == casevidetop)) {
+        if ((caseclickTop + 100 == casevidetop && caseclickLeft == casevideleft) 
+        || (caseclickTop - 100 == casevidetop && caseclickLeft == casevideleft)
+        || (caseclickLeft - 100 == casevideleft && caseclickTop == casevidetop)
+        || (caseclickLeft + 100 == casevideleft && caseclickTop == casevidetop)) {
+            counthit = counthit + 1
+            $('.count').text("Nombre de coup : " + counthit);
 
-        $(this).css("left", casevideleft + 'px');
-        $(this).css("top", casevidetop + 'px')
-
-
-        casevidetop = caseclickTop
-        casevideleft = caseclickLeft
-    }
-
-    console.log($('#case1').position().top)
-    console.log($('#case1').position().left)
-
-    
-    if ($('#case1').position().top == 0 && $('#case1').position().left == 0) {
-        c1good = true
-    } else {
-        c1good = false
-    }
-    if ($('#case2').position().top == 0 && $('#case2').position().left == 100) {
-        c2good = true
-    } else {
-        c2good = false
-    } 
-    if ($('#case3').position().top == 0 && $('#case3').position().left == 200) {
-        c3good = true
-    } else {
-        c3good = false
-    }
-    if ($('#case4').position().top == 0 && $('#case4').position().left == 300) {
-        c4good = true
-    } else {
-        c4good = false
-    }
-    if ($('#case5').position().top == 100 && $('#case5').position().left == 0) {
-        c1good = true
-    } else {
-        c5good = false
-    }
-    if ($('#case6').position().top == 100 && $('#case6').position().left == 100) {
-        c6good = true
-    } else {
-        c6good = false
-    }
-    if ($('#case7').position().top == 100 && $('#case7').position().left == 200) {
-        c1good = true
-    } else {
-        c7good = false
-    }
-    if ($('#case8').position().top == 100 && $('#case8').position().left == 300) {
-        c8good = true
-    } else {
-        c8good = false
-    }
-    if ($('#case9').position().top == 200 && $('#case9').position().left == 0) {
-        c9good = true
-    } else {
-        c9good = false
-    }
-    if ($('#case10').position().top == 200 && $('#case10').position().left == 100) {
-        c10good = true
-    } else {
-        c10good = false
-    }
-    if ($('#case11').position().top == 200 && $('#case11').position().left == 200) {
-        c11good = true
-    } else {
-        c11good = false
-    }
-    if ($('#case12').position().top == 200 && $('#case12').position().left == 300) {
-        c12good = true
-    } else {
-        c12good = false
-    }
-    if ($('#case13').position().top == 300 && $('#case13').position().left == 0) {
-        c13good = true
-    } else {
-        c13good = false
-    }
-    if ($('#case14').position().top == 300 && $('#case14').position().left == 100) {
-        c14good = true
-    } else {
-        c14good = false
-    }
-    if ($('#case15').position().top == 300 && $('#case15').position().left == 200) {
-        c15good = true
-    } else {
-        c15good = false
-    }
-    if ($('#case16').position().top == 300 && $('#case16').position().left == 300) {
-        c16good = true
-    } else {
-        c16good = false
-    }
+            $(this).css("left", casevideleft + 'px');
+            $(this).css("top", casevidetop + 'px')
 
 
-    if (c1good == true && c2good == true && c3good == true && c4good == true && c5good == true && c6good == true && 
-        c7good == true && c8good == true && c9good == true && c10good == true && c11good == true && c12good == true && 
-        c13good == true && c14good == true && c15good == true && c16good == true) {
-        clearInterval(chrono)
-        $('#tableau').hide()
-        $('.victoire').show()
-        lancer = false
-    } 
+            casevidetop = caseclickTop
+            casevideleft = caseclickLeft
+        }
+
+        console.log($('#case1').position().top)
+        console.log($('#case1').position().left)
+
+        
+        if ($('#case1').position().top == 0 && $('#case1').position().left == 0) {
+            c1good = true
+        } else {
+            c1good = false
+        }
+        if ($('#case2').position().top == 0 && $('#case2').position().left == 100) {
+            c2good = true
+        } else {
+            c2good = false
+        } 
+        if ($('#case3').position().top == 0 && $('#case3').position().left == 200) {
+            c3good = true
+        } else {
+            c3good = false
+        }
+        if ($('#case4').position().top == 0 && $('#case4').position().left == 300) {
+            c4good = true
+        } else {
+            c4good = false
+        }
+        if ($('#case5').position().top == 100 && $('#case5').position().left == 0) {
+            c1good = true
+        } else {
+            c5good = false
+        }
+        if ($('#case6').position().top == 100 && $('#case6').position().left == 100) {
+            c6good = true
+        } else {
+            c6good = false
+        }
+        if ($('#case7').position().top == 100 && $('#case7').position().left == 200) {
+            c1good = true
+        } else {
+            c7good = false
+        }
+        if ($('#case8').position().top == 100 && $('#case8').position().left == 300) {
+            c8good = true
+        } else {
+            c8good = false
+        }
+        if ($('#case9').position().top == 200 && $('#case9').position().left == 0) {
+            c9good = true
+        } else {
+            c9good = false
+        }
+        if ($('#case10').position().top == 200 && $('#case10').position().left == 100) {
+            c10good = true
+        } else {
+            c10good = false
+        }
+        if ($('#case11').position().top == 200 && $('#case11').position().left == 200) {
+            c11good = true
+        } else {
+            c11good = false
+        }
+        if ($('#case12').position().top == 200 && $('#case12').position().left == 300) {
+            c12good = true
+        } else {
+            c12good = false
+        }
+        if ($('#case13').position().top == 300 && $('#case13').position().left == 0) {
+            c13good = true
+        } else {
+            c13good = false
+        }
+        if ($('#case14').position().top == 300 && $('#case14').position().left == 100) {
+            c14good = true
+        } else {
+            c14good = false
+        }
+        if ($('#case15').position().top == 300 && $('#case15').position().left == 200) {
+            c15good = true
+        } else {
+            c15good = false
+        }
+        if ($('#case16').position().top == 300 && $('#case16').position().left == 300) {
+            c16good = true
+        } else {
+            c16good = false
+        }
+
+
+        if (c1good == true && c2good == true && c3good == true && c4good == true && c5good == true && c6good == true && 
+            c7good == true && c8good == true && c9good == true && c10good == true && c11good == true && c12good == true && 
+            c13good == true && c14good == true && c15good == true && c16good == true) {
+            clearInterval(chrono)
+            $('#tableau').hide()
+            $('.victoire').show()
+            lancer = false
+        } 
     }
     
 
