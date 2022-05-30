8a27410a9c1a6a9df05937a454dcd23993d3c431
Arkhin <nabillouh62110@gmail.com>
2022-05-30 12:13:32 +0200

    correction erreur dans le nom de variable

--- taquin.js
+++ taquin.js
@@ -60,7 +60,7 @@ $('#debut').click(function() {
         }
         $('.chrono').text('Temps : ' + min + ":" + sec)
     
-    }, 100)
+    }, 1000)
 
 
 
@@ -139,7 +139,15 @@ $('.case').click(function() {
         caseclickTop = $(this).position().top
         caseclickLeft = $(this).position().left
 
-        
+       /* counthit = counthit + 1
+        $('.count').text("Nombre de coup : " + counthit);
+
+        $(this).css("left", casevideleft + 'px');
+        $(this).css("top", casevidetop + 'px')
+
+
+        casevidetop = caseclickTop
+        casevideleft = caseclickLeft*/
 
 
         if ((caseclickTop + 100 == casevidetop && caseclickLeft == casevideleft) 
@@ -182,7 +190,7 @@ $('.case').click(function() {
             c4good = false
         }
         if ($('#case5').position().top == 100 && $('#case5').position().left == 0) {
-            c1good = true
+            c5good = true
         } else {
             c5good = false
         }
@@ -192,7 +200,7 @@ $('.case').click(function() {
             c6good = false
         }
         if ($('#case7').position().top == 100 && $('#case7').position().left == 200) {
-            c1good = true
+            c7good = true
         } else {
             c7good = false
         }
@@ -236,33 +244,60 @@ $('.case').click(function() {
         } else {
             c15good = false
         }
-        if ($('#case16').position().top == 300 && $('#case16').position().left == 300) {
+        if ($('#case16').position().top == 0 && $('#case16').position().left == 0) {
             c16good = true
         } else {
             c16good = false
         }
 
 
+
+       
+
+
+        console.log("c1 est " + c1good)
+        console.log("c2 est " + c2good)
+        console.log("c3 est " + c3good)
+        console.log("c4 est " + c4good)
+        console.log("c5 est " + c5good)
+        console.log($('#case5').position().top)
+        console.log($('#case5').position().left)
+        console.log("c6 est " + c6good)
+        console.log("c7 est " + c7good)
+        console.log($('#case7').position().top)
+        console.log($('#case7').position().left)
+        console.log("c8 est " + c8good)
+        console.log("c9 est " + c9good)
+        console.log("c10 est " + c10good)
+        console.log("c11 est " + c11good)
+        console.log("c12 est " + c12good)
+        console.log("c13 est " + c13good)
+        console.log("c14 est " + c14good)
+        console.log("c15 est " + c15good)
+        console.log("c16 est " + c16good)
+        console.log($('#case16').position().top)
+        console.log($('#case16').position().left)
+
+        
         if (c1good == true && c2good == true && c3good == true && c4good == true && c5good == true && c6good == true && 
             c7good == true && c8good == true && c9good == true && c10good == true && c11good == true && c12good == true && 
-            c13good == true && c14good == true && c15good == true && c16good == true) {
+            c13good == true && c14good == true && c15good == true) {
             clearInterval(chrono)
             $('#tableau').hide()
             $('.victoire').show()
             lancer = false
         } 
+
+
+       
+        
+
     }
     
 
-/*
-    if (c1good == true) {
-        
-        $('#tableau').hide()
-        $('.victoire').show()
-        clearInterval(chrono)
-        lancer = false
-    }
-*/
+
+   
+
 
     
    
